3bedc0cf69adc6566fed3f663d29c4fc82b8fff9
Waheed Ahmad <waheedahmad5519@gmail.com>
2024-10-28 00:25:34 +0500

    Classes & Objects

--- 08_OOP/1_Classes_&_Objects.py
+++ 08_OOP/1_Classes_&_Objects.py
@@ -3,13 +3,17 @@
 # Example : if we make a student management system of a school using classes and objecs 
 # we can make a class for "sudent" and make objects like their "makrs","roll no","subjects" etc 
 class Student:           # class name start with class keyword
+    
     name = "Waheed"      # name of every student is Waheed
+    Roll_no = 2275 
                         
 s1=Student()             # s1 is an object
 print(s1.name)           # use object by " . " like .name
+print(s1.Roll_no)
 
 s2=Student()              #creates an instance (object) of the Car class.
-print(s2.name)           
+print(s2.name)  
+print(s2.Roll_no)         
 
 class Car:              # we declare a class as "car"
     brand = "BMW"          
@@ -17,6 +21,6 @@ class Car:              # we declare a class as "car"
     color = "Black"        
 
 car=Car()            #creates an instance (object) of the Car class.
-print(car.brand)      
-print(car.model)       #print the object using " . "  
-print(car.color)
+print(f"Car Brand :{car.brand}")      
+print(f"Car model :{car.model}")       #print the object using " . "  
+print(f"Car Color :{car.color}")
--- 08_OOP/3_Methods_in_OOP.py
+++ 08_OOP/3_Methods_in_OOP.py
@@ -1,10 +1,18 @@
 # Methods are functions that belong to objects.
+# Define Student class
 class Student:
-    def __init__(self,name,marks):
-        self.name=name
-        self.marks=marks
-    def welcome(self):
-        print("")
-s1=Student("Waheed",88)
-print(f"Student's Name :{s1.name}")   
-print(f"Student's Marks :{s1.marks}")   
+        
+    # Constructor to initialize name and marks
+    def __init__(self, name, marks):
+        self.name = name        # Set the student's name
+        self.marks = marks      # Set the student's marks
+
+    def welcome(self):           # Method to display a welcome message
+        print("Hello Welcome Students!")  # Print welcome message
+
+# Create a Student instance with name "Waheed" and marks 88
+s1 = Student("Waheed", 88)
+s1.welcome()                 # Call the welcome method
+
+print(f"Student's Name: {s1.name}")    # Print the student's name and marks
+print(f"Student's Marks: {s1.marks}")   
