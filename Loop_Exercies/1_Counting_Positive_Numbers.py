f54659c506738add1a02e1bf7a172f01fe630070
Waheed Ahmad <waheedahmad5519@gmail.com>
2024-11-01 10:51:33 +0500

    Loop Ex # Done!

--- Loop_Exercies/1_Counting_Positive_Numbers.py
+++ Loop_Exercies/1_Counting_Positive_Numbers.py
@@ -2,3 +2,11 @@
 # Problem: Given a list of numbers, count how many are positive.
 # numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
 
+# Given list of numbers
+numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+
+# Count the positive numbers using list comprehension
+positive_count = sum(1 for num in numbers if num > 0)
+
+# Output the result
+print("Number of positive numbers:", positive_count)
