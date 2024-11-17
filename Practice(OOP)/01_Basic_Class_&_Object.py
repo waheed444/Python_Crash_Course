403b268be181f104d66bc05088cb06589c80e3e8
Waheed Ahmad <waheedahmad5519@gmail.com>
2024-11-17 22:23:18 +0500

    OOP Practice !!!

--- Practice(OOP)/01_Basic_Class_&_Object.py
+++ Practice(OOP)/01_Basic_Class_&_Object.py
@@ -1,3 +1,5 @@
+#  Basic Class and Object
+# Problem: Create a Car class with attributes like brand and model. Then create an instance of this class.
 class Car:
     def __init__(self, brand,model):
       self.brand = brand
