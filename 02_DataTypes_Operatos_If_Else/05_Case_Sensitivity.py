b9e714ca134d1874e99ec80bb8e64b75aef93a2b
Waheed Ahmad <waheedahmad5519@gmail.com>
2025-02-17 22:07:29 +0500

    Update done!

--- 02_DataTypes_Operatos_If_Else/05_Case_Sensitivity.py
+++ 02_DataTypes_Operatos_If_Else/05_Case_Sensitivity.py
@@ -1,6 +1,9 @@
 #Pthon is the case sensitive langauge.
+
 #It means it effects the code with langauge case (lower/upper case)
+
 #In Python 'name' & 'Name' both are two different variable.
+
 #Similarly 'Print' is varable and 'print' is the keyword
 
 print("This is the print keyword,it used to print the statement ")
--- 03_Lists_&_Tuples/01_Lists.py
+++ 03_Lists_&_Tuples/01_Lists.py
@@ -7,6 +7,7 @@ print(len(num))  #print lenth of all elements in the list
 print(type(num)) #print the type of the list
 
 #list can contains integers,numbers,alphabets,strings,multiple data types
+
 name=["ali","waheed","hassan","ahmad"]
 #index  0      1        2        3
 print(name)
@@ -14,7 +15,9 @@ print(name[0])   #print the 0 index
 print(name[1])   #print the 1 index 
 print(name[2])   #print the 2 index 
 print(name[3])   #print the 3 index 
+
 #print(name[4])   #python  shows the error because 4 index  not  found
+
 #we can also conditional conditions  in list 
 if 16 in num:
     print("Yes")
