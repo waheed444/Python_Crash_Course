d4a4d6e0d7213313a316d511a8a675edef23ed98
Waheed Ahmad <waheedahmad5519@gmail.com>
2025-03-09 07:08:04 +0500

    Set & Methods

--- 04_Dictionary_&_Set/03_Sets.py
+++ 04_Dictionary_&_Set/03_Sets.py
@@ -2,10 +2,11 @@
 #  They store multiple items in a single variable.
 #  Set items are separated by commas and enclosed within curly brackets {}
 # The order is always different for each run(not specified/unordered)
-s={1,3,4,56,6,6,5,4,55,24,55,4,}
-print(s)
+set(key, value, coded_value)={1,3,4,56,6,6,5,4,55,24,55,4,}
+print(set)
+
 #use for loop to print the values in the set
-for value in s:
+for value in set:
     print(value)
 
 # empty set
@@ -13,7 +14,7 @@ waheed={}                         #This is empty dictionary not empty set
 print(type(waheed))
 
 ali=set()
-print(type(ali))                     #this is empty set 
+print(type(ali))                   #This is empty set 
 
 info = {"name", 19, False, 5.9, 19}
 print(info)
--- 04_Dictionary_&_Set/04_Sets_Methods.py
+++ 04_Dictionary_&_Set/04_Sets_Methods.py
@@ -1,11 +1,20 @@
 #Sets in python more or less work in the same way as sets in mathematics.
 #We can perform operations like union and intersection on the sets just like in mathematics.
 
-set1={1,2,34,5,6,56,5,3,4,3,43,55,46,45,43,43}
-set2={33,76,2,43,43,3,43,4,6,54,64,67,56,7,67,4,53,}
-set3=set1.union(set2)   #collect all element of both sets (print once repeated elements)
+set1 = {1,2,34,5,6,56,5,3,4,3,43,55,46,45,43,43}
+
+set2 = {33,76,2,43,43,3,43,4,6,54,64,67,56,7,67,4,53,}
+
+#collect all element of both sets (print once repeated elements)
+
+set3 = set1.union(set2)   
+
 print(set3)
-set3=set1.intersection(set2)      #print common elements of both sets
+
+#print common elements of both sets
+
+set3 = set1.intersection(set2)      
+
 print(set3)
 
 #note: We cannot store dictionary and list in set
