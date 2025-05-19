74dce4ece995eed10f4e3b33314aef74437a2678
Waheed Ahmad <waheedahmad5519@gmail.com>
2025-05-19 07:51:12 +0500

    Constructor

--- 08_OOP_(Classes_&_Objects)/04_Constructor_Exercise.py
+++ 08_OOP_(Classes_&_Objects)/04_Constructor_Exercise.py
@@ -1,16 +1,16 @@
 # Create student class that takes name & marks of 3 subjects as arguments in constructor.
 # Then create a method to print the average.
 
-class Student:                                 # declar a class as Student
-    def __init__(self,name,marks):             # define parametrized construction
+class Student:                          # declar a class as Student
+    def __init__(self,name,marks):      # define parametrized construction
         self.name=name
         self.marks=marks
-    def avg(self):                             #def average function as avg()
+    def avg(self):                      #def average function as avg()
         sum=0
-        for value in self.marks:               #using for loop to get the average
+        for value in self.marks:        #using for loop to get the average
             sum +=value
         print(self.name,"Average is :", sum/3)
             
-s1=Student("Waheed",[77,85,60])              #store marks in the form of list    
-s1.avg()                                     #Print the Average of three marks
+s1=Student("Waheed",[77,85,60])        #store marks in the form of list    
+s1.avg()                               #Print the Average of three marks
         
